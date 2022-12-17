7ab42801854ec3d438925ee8bb2dba3868dbc2e2
Sam <58564925+sambhavvinayak@users.noreply.github.com>
2022-12-17 08:47:56 -0500

    Create Solutions.java

---
+++ Solutions.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solutions{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int n = input.nextInt();
+        
+        if (((n%2)==1)||((n%2)==0)&& n>=6 && n<=20) {
+            System.out.println("Weird");
+        
+        }else if((n%2)==0 && n>=2 && n<=5 || (n%2)==0 && n>=20){
+            System.out.println("Not Weird");
+        }
+        
+    }
+}
